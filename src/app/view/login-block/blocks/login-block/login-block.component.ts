1b79a1ff7f4ebaee41d621031b9dc2776b8ede9b
Aleksandr <alokil70@gmail.com>
2021-09-07 16:22:05 +0300

    login block

--- src/app/view/login-block/blocks/login-block/login-block.component.ts
+++ src/app/view/login-block/blocks/login-block/login-block.component.ts
@@ -14,6 +14,6 @@ export class LoginBlockComponent implements OnInit {
 
   onLogin(value) {
     console.log('onLogin', value);
-    this.serverError += 'ok ';
+    this.serverError += 'error';
   }
 }
